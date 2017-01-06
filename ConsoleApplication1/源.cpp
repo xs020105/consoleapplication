8033a2f2194522f6221878ad93a76a6914e617c0
ynf <xs020105@gmail.com>
2017-01-06 12:54:42 +0800

    页式存储

--- ConsoleApplication1/源.cpp
+++ ConsoleApplication1/源.cpp
@@ -1,196 +1,175 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#define MAXJOB 200		//定义存储数据最大值
+#define maxsize 200		//定义存储数据最大值
+
 typedef struct node
 {
-	int start = 0;//起始地址
-	int length = 0;		    //长度
-	int tag = -1;		//标志被谁占用,若没有占用则设为-1
+	int timein = maxsize;//进入时间
+	int no = -1;//页面序列
+	int nextuse = 0;
 }job;
+int queue[maxsize];
+job memory[maxsize];
 
-typedef struct wt
+void FIFO(int mmax, int duilie)//置换时置换先进的
 {
-	int no;//序号
-	int jcno;//进程号
-	int pv = 0;//申请或释放操作
-	int cout = 0;//容量
-}wai;
-
-job kj[MAXJOB];  //定义空闲区表
-int kjk_num = 0;		//空闲区块数
-int cout = 1;//输出做标记用
-wai wait;
-
-void view()
-{
-	int i, tmp;
-	printf("%d/", cout++);
-	for (i = 0; i<kjk_num; i++) {
-		tmp = kj[i].tag;
-		if (kj[i].tag == -1) {
-			tmp = 0;
-			printf("%d-%d.%d", kj[i].start, kj[i].start + kj[i].length - 1, tmp);
+	int j = 0,flag=0,cout=0,time=0,tmp,tmptime;
+	for(int j=0;j<duilie;j++) {
+		flag = 0;
+		time++;
+		int mintimein = maxsize;
+		for (int i = 0; i < mmax; i++) {
+			if (queue[j] == memory[i].no) {//如果内存中已经有了
+				flag = 1;
+				break;
+			}
+			else {
+				if (memory[i].no == -1) {//如果还没有占用 
+					memory[i].no = queue[j];
+					memory[i].timein = time;
+					cout++;
+					flag = 2;
+					break;
+				}
+				else {
+					if (memory[i].timein < mintimein) {//如果是先进的
+						mintimein = memory[i].timein;
+						tmp = i;
+					}
+				}
+			}
 		}
-		else {
-			printf("%d-%d.1.%d", kj[i].start, kj[i].start + kj[i].length - 1, tmp);
+		if (flag == 0) {
+			memory[tmp].no = queue[j];
+			memory[tmp].timein = time;
+			cout++;
 		}
-		if (i != kjk_num - 1)
+		for (int z = 0; z < mmax; z++) {
+			if (memory[z].no != -1) {
+				printf("%d,", memory[z].no);
+			}
+			else {
+				printf("-,");
+			}
+		}
+		if (flag == 2)
+			flag = 0;
+		printf("%d", flag);
+		if (j < duilie - 1)
 			printf("/");
 	}
 	printf("\n");
+	printf("%d\n", cout);
 }
-
-//首次适应
-void earliest(int job_name, int job_length)
-{
-	int i, j;
-	for (i = 0; i<kjk_num; i++) {				//顺序查找满足条件的空间
-		if (kj[i].length >= job_length&&kj[i].tag == -1) {//用找到的第一个
-			if (kj[i].length != job_length) {//用了以后有剩余
-				kjk_num++;//多了一个空间块
-				for (j = kjk_num; j > i + 1; j--) {
-					kj[j] = kj[j - 1];//由于要分空间块，所以将所有空间块后移一个腾出空间
-				}
-				kj[i].tag = job_name;//记录下进程号
-				kj[i + 1].length = kj[i].length - job_length;//初始化剩余空间的长度
-				kj[i + 1].tag = -1;
-				kj[i].length = job_length; //原空间块的大小就变成了用掉的大小
-				kj[i + 1].start = kj[i].start + job_length;//初始化后一个的起点
-			}
-			else {//用了以后正好没有剩余
-				kj[i].tag = job_name;//记录占用的进程号即可
-			}
-			break;
+bool has(int tmp,int mmax) {//看看内存中有没有这个页面
+	for (int n = 0; n < mmax; n++) {
+		if (memory[n].no == tmp) {
+			return true;
 		}
 	}
+	return false;
 }
-
-//最佳适应算法
-void excellent(int job_name, int job_length)
+void OPT(int mmax, int duilie)//置换时置换先进的
 {
-	int i, j, minleave = 100000, mintag = -1,flag=0;
-	for (i = 0; i<kjk_num; i++) {				//顺序查找满足条件且剩余空间最小的空间块
-		if (kj[i].length >= job_length&&kj[i].tag == -1) {//找到符合条件的了，判断是否最小
-			if (kj[i].length == job_length) {//用了以后正好没有剩余
-				kj[i].tag = job_name;//记录占用的进程号即可
-				return;
-			}
-			else {
-				if (kj[i].length - job_length < minleave) {
-					minleave = kj[i].length - job_length;
-					mintag = i;
-					flag = 1;
-				}
-			}
-
-		}
-	}//跑完循环以后在最小的上分配空间
-
-	if (flag) {
-		kjk_num++;//多了一个空间块
-		for (j = kjk_num; j > mintag + 1; j--) {
-			kj[j] = kj[j - 1];//由于要分空间块，所以将所有空间块后移一个腾出空间
-		}
-		kj[mintag].tag = job_name;//记录下进程号
-		kj[mintag + 1].length = kj[mintag].length - job_length;//初始化剩余空间的长度
-		kj[mintag + 1].tag = -1;
-		kj[mintag].length = job_length; //原空间块的大小就变成了用掉的大小
-		kj[mintag + 1].start = kj[mintag].start + job_length;//初始化后一个的起点
-	}
-}
-
-//最坏适应算法
-void worst(int job_name, int job_length)
-{
-	int i, j, maxleave = -1, maxtag = -1,flag=0;
-	for (i = 0; i<kjk_num; i++) {				//顺序查找满足条件且剩余空间最大的空间块
-		if (kj[i].length >= job_length&&kj[i].tag == -1) {//找到符合条件的了，判断是否最大
-			if (kj[i].length - job_length > maxleave) {
-				maxleave = kj[i].length - job_length;
-				maxtag = i;
+	int j = 0, flag = 0, cout = 0, time = 0, tmp, tmpi,tnext[maxsize];
+	for (int j = 0; j<duilie; j++) {
+		flag = 0;
+		time++;
+		for (int i = 0; i < mmax; i++) {
+			if (queue[j] == memory[i].no) {//如果内存中已经有了
 				flag = 1;
-			}
-		}
-	}//跑完循环以后分配空间
-	if (flag) {
-		for (j = kjk_num; j > maxtag + 1; j--) {
-			kj[j] = kj[j - 1];//由于要分空间块，所以将所有空间块后移一个腾出空间
-		}
-		kj[maxtag].tag = job_name;//记录下进程号
-		kj[maxtag + 1].length = kj[maxtag].length - job_length;//初始化剩余空间的长度
-		kj[maxtag + 1].tag = -1;
-		kj[maxtag].length = job_length; //原空间块的大小就变成了用掉的大小
-		kj[maxtag + 1].start = kj[maxtag].start + job_length;//初始化后一个的起点
-		kjk_num++;//多了一个空间块
-	}
-}
-
-void connect(int a, int b) {//调用的时候必须a<b
-	kj[a].length += kj[b].length;
-	for (int i = b; i + 1 < kjk_num; i++) {
-		kj[i] = kj[i + 1];
-	}
-	kjk_num--;
-}
-
-void free(int jcno,int room) {
-	int flag = 0;
-	for (int i = 0; i < kjk_num; i++) {
-		if (kj[i].tag == jcno&&kj[i].length==room) {//找到该进程的占用块
-			kj[i].tag = -1;//标记为空闲
-			if (i - 1 >= 0 && kj[i - 1].tag == -1) {//如果可以和前一个连起来
-				connect(i - 1, i);
-				i--;
-				flag = 1;//连接了，少了一个
-			}
-			if (flag == 1) {
-				if (i + 1 <= kjk_num&&kj[i + 1].tag == -1) {
-					connect(i, i + 1);
-				}
+				break;
 			}
 			else {
-				if (i + 1 <= kjk_num&&kj[i + 1].tag == -1) {
-					connect(i, i + 1);
+				if (memory[i].no == -1) {//如果还没有占用 
+					memory[i].no = queue[j];
+					memory[i].timein = time;
+					cout++;
+					flag = 2;
+					break;
 				}
 			}
-			break;
 		}
+		if (flag == 0) {
+			for (int x = 0; x < maxsize; x++) {
+				tnext[x] = maxsize;
+			}
+			for (int p = time; p < duilie; p++) {
+				for (int q = 0; q < mmax; q++) {
+					if (memory[q].no == queue[p]) {//如果在后边队列中找到了内存中的页面，那就记录下一次的时间
+						if (p < tnext[memory[q].no]) {
+							tnext[memory[q].no] = p;//tnext中记录了页面下一次最近调用的时间
+						}
+					}
+				}
+			}
+
+			int longnext = 0,tmpcout=0;
+			for (int m = 0; m < maxsize; m++) {
+				if (has(m,mmax)) {
+					if (tnext[m] == maxsize) {
+						tmpcout++;
+					}
+				}
+				if (tnext[m] > longnext&&has(m,mmax)) {//找到队列里最久未用的那个
+					longnext = tnext[m];
+					tmpi = m;//tmpi记录了应该要替换的页面号
+				}
+			}
+			if (longnext == maxsize) {
+			}
+			for (int n = 0; n < mmax; n++) {
+				if (memory[n].no == tmpi) {
+					tmp = n;
+					break;
+				}
+			}
+			memory[tmp].no = queue[j];
+			memory[tmp].timein = time;
+			cout++;
+		}
+		for (int z = 0; z < mmax; z++) {
+			if (memory[z].no != -1) {
+				printf("%d,", memory[z].no);
+			}
+			else {
+				printf("-,");
+			}
+		}
+		if (flag == 2)
+			flag = 0;
+		printf("%d", flag);
+		if (j < duilie - 1)
+			printf("/");
+
 	}
+	printf("\n");
+	printf("%d\n", cout);
 }
 
 int main() {
-	int n, i = 0;
+	memset(queue, -2, sizeof(queue));
+	int n, i = 0,kuaishu;
 	scanf("%d\n", &n);
-	scanf("%d\n", &kj[i].length);
-	kjk_num++;
-	while (~scanf("%d/%d/%d/%d", &wait.no, &wait.jcno, &wait.pv, &wait.cout)) {
-		getchar();
-		switch (n) {
-		case 1:
-			if (wait.pv == 1)
-				earliest(wait.jcno, wait.cout);
-			if (wait.pv == 2)
-				free(wait.jcno, wait.cout);
-			view();
+	scanf("%d\n", &kuaishu);
+	while (1) {
+		scanf("%d", &queue[i++]);
+		if (getchar() == '\n')
 			break;
-		case 2:
-			if (wait.pv == 1)
-				excellent(wait.jcno, wait.cout);
-			if (wait.pv == 2)
-				free(wait.jcno, wait.cout);
-			view();
-			break;
-		case 3:
-			if (wait.pv == 1)
-				worst(wait.jcno, wait.cout);
-			if (wait.pv == 2)
-				free(wait.jcno, wait.cout);
-			view();
-			break;
-		default:
-			break;
-		}
+	}
+	switch (n) {
+	case 1:
+		OPT(kuaishu, i);
+		break;
+	case 2:
+		FIFO(kuaishu, i);
+		break;
+	case 3:
+
+		break;
+	default:
+		break;
 	}
 	return 0;
 }
